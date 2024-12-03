04fcf5d87f01dec585c5f6bf2862420a70449483
vince <vincent.verhoefen@t-online.de>
2024-12-03 20:10:44 +0100

    day 2 part 1 semi-final

--- day02/Main2.java
+++ day02/Main2.java
@@ -1,14 +1,21 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Main2 {
+    public static String TEST_INPUT_FILE_NAME = "day02/test_input.txt";
+    public static String REAL_INPUT_FILE_NAME = "day02/input.txt";
+
     public static void main(String[] args) {
         printList(List());
         System.out.println(checkArrays());
     }
 
-    public static void printList(List<int[]> lineArrays){
+    public static void printList(List<int[]> lineArrays) {
         System.out.println("Line Arrays:");
         for (int[] array : lineArrays) {
             for (int num : array) {
@@ -19,9 +26,9 @@ public class Main2 {
     }
 
 
-    public static List<int[]> List(){
+    public static List<int[]> List() {
 
-        String filePath = "day02/test_input.txt";
+        String filePath = TEST_INPUT_FILE_NAME;
         List<int[]> lineArrays = new ArrayList<>();
 
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
@@ -40,24 +47,47 @@ public class Main2 {
         return lineArrays;
     }
 
-    public static int[] grabArray(List<int[]> lineArrays, int indexToAccess){
-        int[] selectedArray = lineArrays.get(indexToAccess);
-        return selectedArray;
+    public static int[] grabArray(List<int[]> lineArrays, int indexToAccess) {
+        return lineArrays.get(indexToAccess);
     }
 
-    public static int checkArrays(){
-        int safeness=0;
-        for(int i = 0; i < List().size(); i++){
-            for(int j = 1; j < grabArray(List(), i).length; j++){
-                int diff = grabArray(List(), i)[j] - grabArray(List(), i)[j-1];
-                if(Math.abs(diff)<1 || Math.abs(diff)>3){
-                    System.out.println(i+" "+j+" "+diff);
-                    safeness--;
+    public static int checkArrays() {
+        int safeness = 0;
+        for (int i = 0; i < List().size(); i++) {
+            int[] row = grabArray(List(), i);
+
+            if (!checkIfArrayIsAscendingOrDescending(row)) {
+                continue;
+            }
+
+            boolean isSafe = true;
+            for (int j = 1; j < row.length; j++) {
+                int diff = row[j - 1] - row[j];
+                int absDiff = Math.abs(diff);
+
+                if (absDiff < 1 || absDiff > 3) {
+                    isSafe = false;
                     break;
                 }
             }
-            safeness++;
+            if (isSafe) safeness++;
         }
         return safeness;
+    }
+
+    public static boolean checkIfArrayIsAscendingOrDescending(int[] array) {
+        Integer[] baseArray = cloneIntArrayToBoxedArray(array);
+
+        Integer[] descendingArray = cloneIntArrayToBoxedArray(array);
+        Arrays.sort(descendingArray);
+
+        Integer[] ascendingArray = cloneIntArrayToBoxedArray(array);
+        Arrays.sort(ascendingArray, Collections.reverseOrder());
+
+        return (Arrays.equals(baseArray, descendingArray) || Arrays.equals(baseArray, ascendingArray));
+    }
+
+    public static Integer[] cloneIntArrayToBoxedArray(int[] array) {
+        return Arrays.stream(array).boxed().toArray(Integer[]::new);
     }
 }
