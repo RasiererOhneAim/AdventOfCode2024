00d0dfa1eb4726130fea51f193b0b4e93e755637
Vincent Verhöfen <vincent.verhoefen@t-online.de>
2025-05-25 23:42:18 +0200

    added day 6 reading input

--- day06/Day6.java
+++ day06/Day6.java
@@ -1,12 +1,28 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Day6 {
-    public static String TEST_INPUT_FILE = "day05/test_input.txt";
-    public static String REAL_INPUT_FILE = "day05/input.txt";
+    public static String TEST_INPUT_FILE = "day06/test_input.txt";
+    public static String REAL_INPUT_FILE = "day06/input.txt";
 
     public static void main(String[] args) throws IOException {
         System.out.println("Result part 1: " + part1());
         System.out.println("Result part 2: " + part2());
+        System.out.println(lines());
+    }
+
+    public static List<String> lines() throws IOException {
+        String filePath = TEST_INPUT_FILE;
+        BufferedReader br = new BufferedReader(new FileReader(filePath));
+        List<String> lineArrays = new ArrayList<>();
+        String line;
+        while ((line = br.readLine()) != null) {
+            lineArrays.add(line);
+        }
+        return lineArrays;
     }
 
     public static int part1() throws IOException{
