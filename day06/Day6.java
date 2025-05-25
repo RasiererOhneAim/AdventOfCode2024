e6dee65bd64de2133cc520c20394e073f954ac8a
Vincent Verhöfen <vincent.verhoefen@t-online.de>
2025-05-25 23:27:46 +0200

    added day 6 inputs

--- day06/Day6.java
+++ day06/Day6.java
@@ -1,2 +1,20 @@
+import java.io.IOException;
+
 public class Day6 {
+    public static String TEST_INPUT_FILE = "day05/test_input.txt";
+    public static String REAL_INPUT_FILE = "day05/input.txt";
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Result part 1: " + part1());
+        System.out.println("Result part 2: " + part2());
+    }
+
+    public static int part1() throws IOException{
+        return 0;
+    }
+
+    public static int part2() throws IOException{
+        return 0;
+    }
+
 }
